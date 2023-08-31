ffd7b113eb7abf6399dd64058eb562f7304e4ab3
Samuel Shapley <samuelshapley@gmail.com>
2023-08-31 21:00:25 +0100

    tools 6

--- helpers.py
+++ helpers.py
@@ -1,6 +1,12 @@
 import yaml
 import matplotlib.pyplot as plt
 
+# Load the configuration
+with open('config.yml', 'r') as f:
+    config = yaml.safe_load(f)
+
+tools_enabled = config['tools_enabled']
+
 def load_pipeline(file_path):
     """Load a pipeline configuration from a YAML file."""
     print("\033[93mLoading pipeline...\033[00m")
@@ -12,6 +18,11 @@ def load_system_prompt(module_name):
     # Load the generic system prompt
     with open('general_system.txt', 'r') as file:
         system_prompt = file.read().replace('\n', '')
+
+    if tools_enabled:
+        with open(f'tools/tool_prompt.txt', 'r') as file:
+            tool_prompt = file.read().replace('\n', '')
+        system_prompt += '\n\n' + tool_prompt + '\n'
 
     with open(f'system_prompts/{module_name}.txt', 'r') as file:
         module_prompt = file.read().replace('\n', '')
